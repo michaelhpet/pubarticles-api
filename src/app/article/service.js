a0167af753484bf4b2518d3330393963cd79f2d7
Michael Peter <michaelhpet@gmail.com>
2024-05-05 11:46:55 +0100

    fix: make title unique

--- src/app/article/service.js
+++ src/app/article/service.js
@@ -3,12 +3,12 @@ const Article = require("./model");
 
 class ArticleService {
   async createArticle(payload) {
+    if (await Article.findOne({ title: payload.title }))
+      throw new AppError(400, "Article with this title already exists");
     return await Article.create(payload);
   }
 
   async getArticles(payload) {
-    if (await Article.findOne({ title: payload.title }))
-      throw new AppError(400, "Article with this title already exists");
     const { page = 1, limit = 10, search, ...query } = payload;
     const offset = (page - 1) * limit;
     const articles = await Article.find({
