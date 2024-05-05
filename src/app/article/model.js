89505e172a624a9f95b54de0f1032e82b5dc9c56
Michael Peter <michaelhpet@gmail.com>
2024-05-05 08:03:09 +0100

    fix: resolve search index error

--- src/app/article/model.js
+++ src/app/article/model.js
@@ -4,11 +4,12 @@ const articleSchema = new mongoose.Schema({
   title: { type: String, required: true, index: true },
   excerpt: { type: String },
   body: { type: String, required: true },
-  author_name: { type: String, required: true, index: true },
+  author_name: { type: String, required: true },
   author_email: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
 });
 
+articleSchema.index({ title: "text", author_name: "text" });
 const Article = mongoose.model("Article", articleSchema);
 
 module.exports = Article;
--- src/app/article/service.js
+++ src/app/article/service.js
@@ -1,3 +1,4 @@
+const { AppError } = require("../../utils");
 const Article = require("./model");
 
 class ArticleService {
@@ -6,6 +7,8 @@ class ArticleService {
   }
 
   async getArticles(payload) {
+    if (await Article.findOne({ title: payload.title }))
+      throw new AppError(400, "Article with this title already exists");
     const { page = 1, limit = 10, search, ...query } = payload;
     const offset = (page - 1) * limit;
     const articles = await Article.find({
